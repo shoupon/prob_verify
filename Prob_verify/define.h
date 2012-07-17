9813b917a3217735532b3117a0e45c5860946f67
shouponlin <shoupon.lin@gmail.com>
2012-07-17 16:56:34 -0400

    Created FSMs for full-duplex ABP (updated)

--- Prob_verify/define.h
+++ Prob_verify/define.h
@@ -4,8 +4,8 @@ using namespace std;
 #ifndef DEFINE_H
 #define DEFINE_H
 
-#define HALF_DUPLEX
-//#define FULL_DUPLEX
+//#define HALF_DUPLEX
+#define FULL_DUPLEX
 
 typedef map<string,int> Table;
 typedef Table::value_type Entry;
